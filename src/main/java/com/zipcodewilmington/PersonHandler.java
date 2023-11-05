88155864b1c67f6a89ada287c6ab0c0def1311f5
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-11-04 23:14:44 -0400

    finished

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -14,27 +14,33 @@ public class PersonHandler {
     // simple lines of code...
     
     public String whileLoop() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
-
+        int count = 0;
+        while (count < personArray.length){
+            result.append(personArray[count]);
+            count++;
+        }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
 
             // end loop
-        return result;
+        return result.toString();
     }
 
 
 
     public String forLoop() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for(int i = 0; i < personArray.length; i++){
+            result.append(personArray[i]);
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -42,23 +48,25 @@ public class PersonHandler {
                 // append `stringRepresentation` to `result` variable
             // end loop
 
-        return result;
+        return result.toString();
     }
 
 
 
     public String forEachLoop() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         // identify array's type
         // identify array's variable-name
-
+        for (Person person : personArray) {
+            result.append(person);
+        }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
 
-        return result;
+        return result.toString();
     }
 
 
